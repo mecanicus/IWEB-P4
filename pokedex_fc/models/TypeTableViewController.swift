16f9fd9ed94aa038c08739296a33ba52a6769b42
mecanicus <fernandomonjer@gmail.com>
2017-11-29 19:02:16 +0100

    Revert "IWEB-P5"
    This reverts commit 8210193da74c6e8f8c428329dfdeb3ac3a765cd6.

--- pokedex_fc/models/TypeTableViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  TypeTableViewController.swift
-//  pokedex_fc
-//
-//  Created by Carlos Villa on 13/11/2017.
-//  Copyright © 2017 Carlos Fernando. All rights reserved.
-//
-
-import Foundation
